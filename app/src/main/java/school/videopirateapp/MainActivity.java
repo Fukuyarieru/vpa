b4f1c8d4a6018f4c23231ecc78767c3e1f29fc78
Your Name <you@example.com>
2024-11-27 08:33:13 +0200

    A WORKING MESSAGE

--- app/src/main/java/school/videopirateapp/MainActivity.java
+++ app/src/main/java/school/videopirateapp/MainActivity.java
@@ -14,45 +14,46 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference database;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database=FirebaseDatabase.getInstance().getReference();
+        myRef.setValue("A WORKING MESSAGE");
     }
 
-    public void TESTBUTTON(View view) {
-        TestWriteToDatabase();
-    }
-
-    public void TestWriteToDatabase() {
-//        User user=new User("Test","123");
-//        database.child("users").child("user1").setValue(user)
-//                .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                    @Override
-//                    public void onSuccess(Void unused) {
-//                        Toast.makeText(MainActivity.this, "DATA WRITTEN SUCCESFULLY", Toast.LENGTH_LONG).show();
-//                    }
-//                })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                // Show failure message on failed write
-//                                Toast.makeText(MainActivity.this, "FAILED TO WRITE DATA", Toast.LENGTH_LONG).show();
-//                            }
-//                        });
+//    public void TESTBUTTON(View view) {
+//        TestWriteToDatabase();
 //    }
-        // Create a new User object
-        User user = new User("Test", "123");
 
-        // Optionally, add some items to the user's Playlist
-        Playlist playlist = user.getUploads();
-        playlist.addItem("Item1");
-        playlist.addItem("Item2");
-
-        // Set the user object in the Firebase database under the "users" node
-    }
+//    public void TestWriteToDatabase() {
+////        User user=new User("Test","123");
+////        database.child("users").child("user1").setValue(user)
+////                .addOnSuccessListener(new OnSuccessListener<Void>() {
+////                    @Override
+////                    public void onSuccess(Void unused) {
+////                        Toast.makeText(MainActivity.this, "DATA WRITTEN SUCCESFULLY", Toast.LENGTH_LONG).show();
+////                    }
+////                })
+////                        .addOnFailureListener(new OnFailureListener() {
+////                            @Override
+////                            public void onFailure(@NonNull Exception e) {
+////                                // Show failure message on failed write
+////                                Toast.makeText(MainActivity.this, "FAILED TO WRITE DATA", Toast.LENGTH_LONG).show();
+////                            }
+////                        });
+////    }
+//        // Create a new User object
+//        User user = new User("Test", "123");
+//
+//        // Optionally, add some items to the user's Playlist
+//        Playlist playlist = user.getUploads();
+//        playlist.addItem("Item1");
+//        playlist.addItem("Item2");
+//
+//        // Set the user object in the Firebase database under the "users" node
+//    }
 }
