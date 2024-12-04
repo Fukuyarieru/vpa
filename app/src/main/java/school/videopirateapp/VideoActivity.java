31960920b8331a1ded493ccddb57df00acb85041
Your Name <you@example.com>
2024-12-04 14:53:16 +0200

    VIDEOOOOOOOO

--- app/src/main/java/school/videopirateapp/VideoActivity.java
+++ app/src/main/java/school/videopirateapp/VideoActivity.java
@@ -10,15 +10,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class VideoActivity extends AppCompatActivity {
 
+    // activity_video_page.xml
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_video_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // TODO
     }
 }
