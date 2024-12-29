5d6a27112b98dc6a17724976b51059b9521b6ef3
fukuya <fukuyarieru@gmail.com>
2024-12-30 00:47:40 +0200

    hm

--- app/src/main/java/school/videopirateapp/Comment.java
+++ app/src/main/java/school/videopirateapp/Comment.java
@@ -30,21 +30,37 @@ public class Comment{
     public void setId(Integer id) {
         this.id = id;
     }
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
 
 
     private String Comment;
     private String Author;
     private Integer id; // must have an ID field because comment dont have distinct features like names or titles, a proper ID will need to be implemented
+    private String video;
     
-    
-    public Comment(String author, String comment, Integer id) {
+
+    public Comment(String comment, String author, String video, Integer id) {
         this.Comment=comment;
         this.Author=author;
         this.id=id;
+        this.video=video;
+    }
+    public Comment(String comment, String author, Integer id) {
+        this(comment,author,"null",id);
     }
     // Default for empty case
     public Comment() {
-        this("@Default","Lorem ipsum dolor sit amet, consectetur adipiscing elit",0); // User.Default().getName()
+        this("Lorem ipsum dolor sit amet, consectetur adipiscing elit","@Default",0); // User.Default().getName()
     }
     public Comment(String author, String comment) {
         this(author,comment,0);
