f713c2a4d1fcaf45568cf1978c19432c4e2a910e
PzzaZZa <arick.kfn@gmail.com>
2025-01-08 15:46:40 +0200

    e update

--- app/src/main/java/school/videopirateapp/MainMenu_UploadVideo_Dialog_Activity.java
+++ app/src/main/java/school/videopirateapp/MainMenu_UploadVideo_Dialog_Activity.java
@@ -3,12 +3,34 @@ package school.videopirateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainMenu_UploadVideo_Dialog_Activity extends AppCompatActivity {
 
+    ImageView thumbnail;
+    Button btnChooseVideo;
+    Button btnUploadVideo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_upload_video_dialog);
+
+        btnChooseVideo=findViewById(R.id.MainMenu_UploadVideo_Dialog_Button_ChooseVideo);
+        btnUploadVideo=findViewById(R.id.MainMenu_UploadVideo_Dialog_Button_UploadVideo);
+        thumbnail=findViewById(R.id.MainMenu_UploadVideo_Dialog_ImageView_Thumbnail);
+
+
+        btnUploadVideo.setText("Upload");
+        btnChooseVideo.setText("Choose");
+
+        // TODO, finish this
+    }
+    public void ChooseVideo(View view) {
+        // TODO
+    }
+    public void UploadVideo(View view) {
+        // TODO
     }
 }
