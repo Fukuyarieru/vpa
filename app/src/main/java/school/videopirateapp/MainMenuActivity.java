60e756538fea5da1721aba28e728fd0e61b5547d
Your Name <you@example.com>
2025-01-09 12:29:38 +0200

    fix?

--- app/src/main/java/school/videopirateapp/MainMenuActivity.java
+++ app/src/main/java/school/videopirateapp/MainMenuActivity.java
@@ -23,7 +23,7 @@ public class MainMenuActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_menu);
 
-        finishActivity();
+//        finishActivity();
 
 //        startActivityfrom();
 
